c3d41ad95f819db7e2f9d151cc2e3f2d43a544c8
B20-Project <aji.diliyaer@gmail.com>
2020-09-15 11:38:26 -0400

    user story #2 & #3 added

---
+++ src/test/java/com/phptravels/tests/HeaderDropDownModules.java
@@ -0,0 +1,26 @@
+package com.phptravels.tests;
+
+import com.phptravels.Util.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class HeaderDropDownModules {
+    WebDriver driver;
+    String URL = "https://www.phptravels.net/";
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
