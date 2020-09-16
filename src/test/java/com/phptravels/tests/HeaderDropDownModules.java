b06be3ef42921e4280888ed4b69d17661714af7e
elv-krgb <elv.krgb20@gmail.com>
2020-09-16 11:36:55 -0400

    languageBar.java

--- src/test/java/com/phptravels/tests/HeaderDropDownModules.java
+++ src/test/java/com/phptravels/tests/HeaderDropDownModules.java
@@ -1,15 +1,22 @@
 package com.phptravels.tests;
 
 import com.phptravels.Util.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class HeaderDropDownModules {
     WebDriver driver;
     String URL = "https://www.phptravels.net/";
+    String languageBarXpath = "//a[@id='dropdownLangauge']";
+    String listOfLanguagesXpath = "//div[@class='dropdown-menu dropdown-menu-right show']/div/a";
 
     @BeforeMethod
     public void setUp(){
@@ -18,6 +25,30 @@ public class HeaderDropDownModules {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(URL);
     }
+
+    @Test //AC#3 - Language DropDown - verify each language (Elvira)
+    public void language_dropDown(){
+
+        WebElement languageBar = driver.findElement(By.xpath(languageBarXpath));
+        languageBar.click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        String[] expectedListOfLanguages = {"Vietnamese","Russian","English","Arabic","Farsi","Turkish","French","Spanish","German"};
+
+        int i = 0;
+        List<WebElement> listOfLanguages = driver.findElements(By.xpath(listOfLanguagesXpath));
+        for(WebElement each : listOfLanguages){
+            //System.out.println(each.getText());
+            Assert.assertEquals(each.getText(), expectedListOfLanguages[i]);
+                i++;
+        }
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        int sizeOfExpectedListOfLanguages = expectedListOfLanguages.length;
+        int sizeOfListOfLanguages = listOfLanguages.size();
+        Assert.assertTrue(sizeOfExpectedListOfLanguages == sizeOfListOfLanguages);
+    }
+
+
 
     @AfterMethod
     public void tearDown(){
