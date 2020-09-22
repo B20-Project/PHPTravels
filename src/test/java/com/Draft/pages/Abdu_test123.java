d285b995e979961d98d3ff04f82015ab0581a2a6
B20-Project <aji.diliyaer@gmail.com>
2020-09-22 10:46:39 -0400

    first

--- src/test/java/com/Draft/pages/Abdu_test123.java
+++ src/test/java/com/Draft/pages/Abdu_test123.java
@@ -13,6 +13,7 @@ public class Abdu_test123 {
     //123123133333333
     //testtesttesttesttesttest
 
+    //recording live !!!!!!!!!!!!
     public static void main(String[] args) {
 
         System.out.println("sfsfds");
